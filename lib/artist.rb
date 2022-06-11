baf83cad74788f9a097acfa3d483c77493866292
princegobah <babynamek@gmail.com>
2022-06-10 22:06:37 -0400

    commit tree 8c436241d623ac5c45f99e3dfb7ccdf901da4418

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,15 +1,20 @@
 require 'pry'
 
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  
+include Paramable
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+     super 
     @songs = []
-  end
+end
+
 
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
